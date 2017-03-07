e846e312e46ba16fb9a839a2711b2ddf03a9787b
Raghav Karol <rkarol@basho.com>
2017-03-07 10:57:23 +0100

    Convert to escript

--- src/erl_deps.erl
+++ src/erl_deps.erl
@@ -2,13 +2,7 @@
 
 %% API exports
 -export([main/1,
-         main/2,
-         default_opts/0]).
-
--type opt() :: get_deps
-             | no_get_deps.
-
--type opts() :: [opt()].
+         do/1]).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
@@ -17,8 +11,7 @@
 -type dot() :: binary().
 
 -record(app, {name :: atom(),
-              path :: string(),
-              rebar3 :: boolean()}).
+              path :: string()}).
 
 -record(dep, {name :: atom(),
               path :: string()}).
@@ -26,39 +19,73 @@
 -record(app_deps, {app ::  #app{},
                    deps :: [#dep{}]}).
 
+
+-record(args, {fetch_deps = false,
+               out_file = undefined,
+               path_to_app = undefined}).
+
 %%====================================================================
 %% API functions
 %%====================================================================
--spec main(string()) -> term().
-main(App) ->
-    main(App, default_opts()).
+-spec parse_cmd_line(CmdLineArgs :: []) -> #args{} | no_return.
+parse_cmd_line(CmdLineArgs) ->
+    case getopt:parse(opt_spec_list(), CmdLineArgs) of
+        {ok, {ParsedArgs, _Extra}} ->
+            case Args = to_args(ParsedArgs) of
+                #args{out_file = undefined} ->
+                    throw(io_lib:format("Error: missing out-file ~n~n", []));
+                #args{path_to_app = undefined} ->
+                    throw(io_lib:format("Error: missing path-to-app ~n~n", []));
+                Args ->
+                    Args
+            end;
 
--spec main(string(), opts()) -> term().
-main(App, Opts) ->
-    ok = maybe_rebar_get_deps(get_opt(get_deps, Opts),
-                              App),
-    AllDeps = lists:map(fun(Dep) ->
-                                #app_deps{
-                                   app =
-                                       #app{name = filename:basename(Dep),
-                                            path = Dep,
-                                            rebar3 = has_rebar3_feature_branch(Dep)},
-                                   deps = [#dep{name = atom_to_list(DepName),
-                                                path = filename:dirname(App) ++ "/dep/" ++ atom_to_list(DepName)}
-                                           || {DepName, _, _} <- rebar_config_deps(Dep)]}
-                        end,
-                        [App| ls_deps_dir(App)]),
+        {error, {Reason, Data}} ->
+            throw(io_lib:format("Error: ~s ~p~n~n", [Reason, Data]))
+    end.
+
+main(CmdLineArgs) ->
+    try
+        do(parse_cmd_line(CmdLineArgs))
+    catch
+        throw:Error ->
+            io:format("Error: ~s ~n", [Error]),
+            getopt:usage(opt_spec_list(), escript:script_name())
+    end.
+
+path_to_app_dep(AppPath, Path) ->
+    #app_deps{
+       app =
+           #app{name = filename:basename(Path),
+                path = Path},
+       deps = [#dep{name = atom_to_list(DepName),
+                    path = filename:dirname(AppPath) ++ "/dep/" ++ atom_to_list(DepName)}
+               || {DepName, _, _} <- rebar_config_deps(Path)]}.
+
+do(#args{fetch_deps = FetchDeps,
+            out_file = OutFile,
+            path_to_app = AppPath}) ->
+
+    ok = maybe_rebar_get_deps(FetchDeps, AppPath),
+
+    AllDeps = lists:map(fun(Path) -> path_to_app_dep(AppPath, Path)
+                        end, [AppPath| ls_deps_dir(AppPath)]),
     DotData = to_dot_digraph(AllDeps),
-    ok = file:write_file("/Users/raghav/github/erl_deps/" ++ dot_filename(App), DotData).
+    ok = file:write_file(OutFile, DotData).
 
-
-default_opts() ->
-    [get_deps].
 %%====================================================================
 %% Internal functions
 %%====================================================================
-dot_filename(App) ->
-    filename:basename(App) ++ ".dot".
+opt_spec_list() ->
+    [{fetch_deps,    $f,        "fetch-deps",    {boolean, false}, "calls rebar3 deps to fetch deps first"},
+     {out_file,      $o,        "out-file",      string,           "output dot file name"},
+     {path_to_app,   $p,        "path-to-app",   string,           "path the application that should be analyzed"}
+    ].
+
+to_args(Args) ->
+    #args{fetch_deps  = proplists:get_value(fetch_deps, Args, false),
+          out_file    = proplists:get_value(out_file, Args),
+          path_to_app = proplists:get_value(path_to_app, Args) }.
 
 indent() ->
     "  ".
@@ -74,16 +101,14 @@ to_dot_props(#app_deps{} = AppDeps) ->
         ++ style_props(AppDeps)
         ++ [].
 
-style_props(#app_deps{app = #app{rebar3 = true}}) ->
-    [];
-style_props(#app_deps{app = #app{rebar3 = false}}) ->
-    [{style, dashed}].
+style_props(#app_deps{}) ->
+    [].
 
 color_props(#app_deps{deps = []}) ->            % Leaf node
-    [{color, black}];
+    [{color, green}];
 
 color_props(#app_deps{}) ->
-    [{color, red}].
+    [{color, brown}].
 
 default_props() ->
     [{fontname, courier}].
@@ -117,6 +142,7 @@ rebar_config(App) ->
         {ok, Terms} ->
             Terms;
         {error, _} = E->
+            io:format(user, "~n>>> ~s:~p -- ~p~n", [?MODULE, ?LINE, App]),
             io:format(user, "~n>>> ~s:~p -- ~p~n", [?MODULE, ?LINE, E]),
             []
     end.
@@ -140,21 +166,6 @@ run_cmd(Cmd) ->
 cmd_str(Format, Args) ->
     lists:flatten(io_lib:format(Format, Args)).
 
-get_opt(Opt, Opts) ->
-    proplists:get_value(Opt, Opts, undefined).
-
-
--spec has_rebar3_feature_branch(App :: string()) -> boolean().
-has_rebar3_feature_branch(App) ->
-    CmdStr = cmd_str("(cd ~s && git branch -a | grep feature/riak-2903/rebar3)",
-                     [App]),
-    case run_cmd(CmdStr) of
-        [] -> false;
-        _ ->
-            true
-    end.
-
-
 -spec to_iodata([{Key :: atom(), Value :: atom()}]) -> iodata().
 to_iodata(Props) ->
     [
@@ -164,12 +175,27 @@ to_iodata(Props) ->
     ].
 
 
-
 -ifdef(TEST).
 
 to_iodata_test() ->
     R = to_iodata([{fontname, courier}, {color, green}, {style, dotted}]),
     "[fontname = \"courier\", color = \"green\", style = \"dotted\"]" = lists:flatten(R),
     ok.
+
+getopt_test() ->
+    ParsedArgs1 = getopt:parse(opt_spec_list(), getopt:tokenize("-f -o x.dot")),
+    {ok, {[{fetch_deps, true},
+           {out_file,"x.dot"}], []}} = ParsedArgs1,
+
+    ParsedArgs2 = getopt:parse(opt_spec_list(), getopt:tokenize("-o x.dot")),
+    {ok, {[{out_file,"x.dot"},
+           {fetch_deps, false}], []}} = ParsedArgs2,
+
+    ParsedArgs3 = getopt:parse(opt_spec_list(), getopt:tokenize("-o x.dot -f0")),
+    {ok, {[{out_file,"x.dot"},
+           {fetch_deps, false}], []}} = ParsedArgs3,
+
+    ok.
+
 
 -endif.
